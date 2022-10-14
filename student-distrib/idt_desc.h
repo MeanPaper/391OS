13497eb123ddfdb69db3a03db40bcbd6f49de3d5
Elijah Ye <elijah.gaohan.ye@gmail.com>
2022-10-13 23:29:01 -0500

    add macro for linkage

--- student-distrib/idt_desc.h
+++ student-distrib/idt_desc.h
@@ -5,5 +5,16 @@
 // initialize the idt table
 extern void init_idt_desc();
 
+// #define INTR_LINK(name, func)    \
+//     .global name                ;\
+//     name:                       ;\
+//         pushal                  ;\
+//         pushfl                  ;\
+//         call func               ;\
+//         popfl                   ;\
+//         popal                   ;\
+//         iret                    ;\
+
+// INTR_LINK(rtc_handler_linkage, rtc_handler);
 
 #endif
