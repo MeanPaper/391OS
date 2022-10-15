67c66551e495eeb21f0d23f1d98d03a8f1063e60
Jinyu Xu <jinyuxu@wirelessprv-10-193-133-225.near.illinois.edu>
2022-10-15 15:04:08 -0500

    add hash table for kbd

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,8 +1,14 @@
+static uint8_t scancode_hash[59] = 	{'\0', '\0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\0', '\0',
+	 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\0', '\0', 'a', 's',
+	 'd', 'f', 'g', 'h', 'j', 'k', 'l' , ';', '\'', '`', '\0', '\\', 'z', 'x', 'c', 'v', 
+	 'b', 'n', 'm',',', '.', '/', '\0', '*', '\0', ' ', '\0'};
+	 
 void keyboard_init(){
 	enable_irq(KEYBOARD_IRQ);
 }
 
 void keyboard_interrupt(){
-	printf("Typed: %d \n", inb(KEYBOARD_PORT));
+	uint8_t idx = inb(KEYBOARD_PORT)
+	printf("Typed: %d \n", scancode_hash[idx]);
 	send_eoi(KEYBOARD_IRQ);
 }
