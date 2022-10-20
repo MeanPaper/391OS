e39b7bdddbeb088c495416bc7332211e7f8cf22c
Liu <dl35@illinois.edu>
2022-10-20 16:08:07 -0500

    good idea

--- student-distrib/file_system.c
+++ student-distrib/file_system.c
@@ -1,17 +1,32 @@
 #include "file_system.h"
+#include "lib.h"
 
-#define DENTRY_SIZE 64
-#define BLOCK_SIZE  4096
-#define FOUR_BYTE_COUNT 1024
 
-uint32_t * file_system_addr;    // the addr of the file system
-uint32_t total_dentry_num;      // total number of dentry block, the offset is pointer + 0
-uint32_t total_inode_num;       // total number of inodes, the offset is pointer + 1
-uint32_t total_data_block_num;  // total number of data block, the offset is pointer + 2
 
+
+boot_block_t * boot_block;
 void init_file_system(uint32_t* file_system_ptr){
-    file_system_addr = file_system_ptr;
-    total_dentry_num = *(file_system_ptr);
-    total_inode_num = *(file_system_ptr + 1);
-    total_data_block_num = *(file_system_ptr + 2);
+    boot_block = (boot_block_t*) file_system_ptr;
 }
+
+int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry){
+    int i;
+    dentry_t temp;
+    
+    for(i = 0; i < boot_block->total_dentry_num - 1; ++i){
+        // go the next file dentry
+        temp = boot_block->files[i];
+
+
+        // using string compare to compare the file name and the name that it request
+        // read the usage of the function, fname is need to be string 1 since it might be null terminated 
+        if(!strncmp(fname, temp.file_name, strlen(fname))){
+        
+        }
+    }
+    return -1;
+}
+
+
+
+
