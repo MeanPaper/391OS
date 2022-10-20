5f7caed55a7113e196f172fc714f3ef7227af72b
Liu <dl35@illinois.edu>
2022-10-20 11:02:29 -0500

    header file drafting complete

--- student-distrib/file_system.h
+++ student-distrib/file_system.h
@@ -8,6 +8,7 @@
 
 
 
+
 typedef struct dentry{
     uint8_t file_name[32];  // file name is 32 byte
     uint32_t file_type;     // 4 byte
@@ -16,6 +17,20 @@ typedef struct dentry{
 }dentry_t;
 
 
+typedef struct inode
+{   // index 0 of the content: the size of the file = sum(number of bytes used in the data blocks)
+    // index > 0: the data block index number
+    uint32_t content[1024];
+}inode_t;
+
+typedef struct boot_block
+{   
+    uint32_t    total_dentry_num;
+    uint32_t    total_inode_num;
+    uint32_t    total_data_block_num;
+    uint8_t     reserved[52];
+    dentry_t    files[63];  // index 0 of this array is file "."
+}boot_block_t;
 
 extern int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
 
