fb2d9b1f496e7a6419c6f6d0607aeacc005040df
Liu <dl35@illinois.edu>
2022-11-03 12:09:32 -0500

    start writing execute

--- student-distrib/sys_call_helper.c
+++ student-distrib/sys_call_helper.c
@@ -1,6 +1,11 @@
 #include "sys_call_helper.h"
 #include "lib.h"
 
+
+// the magic header of elf
+uint8_t elf_magic[ELF_MAGIC_SIZE] = {0x7f, 0x45, 0x4c, 0x46}; // "DEL,E,L,F"
+
+
 /*
  * system_call_helper 
  * Description: output system call message and put system in loop for now
@@ -15,3 +20,70 @@
 //     while(1);  
 // }
 
+int32_t halt (uint8_t status){
+    // Restore parent data
+    // Restore parent paging
+    // Close any relevant FDs
+    // Jump to execute return
+    return 0;
+}
+
+
+
+
+int32_t execute (const uint8_t* command){
+    dentry_t entry;     // file entry 
+    pcb_t entry_pcb;    // the process block
+    
+    // Parse args
+    if(-1 == read_dentry_by_name(command, &entry)){ // we only consider the command is in one line for now
+        // failed to find the file
+        return -1;                                  
+    }
+    
+    uint8_t elf_buffer[ELF_MAGIC_SIZE];         
+
+    // read the first 4 byte of the file to see if the file has ELF
+    read_data(entry.inode_num, 0, elf_buffer, ELF_MAGIC_SIZE);          
+    if(strncmp((int8_t*)elf_magic,(int8_t*)elf_buffer,ELF_MAGIC_SIZE)){
+        // failed to find magic ELF, the file is not executable
+        return -1;                          
+    }
+
+
+    // Check for executable
+    // Set up paging
+    // Losd file into memory
+    // Create PCB
+    // Prepare for Context Switch
+    // Push IRET context to kernel stack
+    return 0;
+}
+
+
+
+
+int32_t read(int32_t fd, void* buf, int32_t nbytes){
+    
+}
+
+
+
+
+int32_t write(int32_t fd, const void* buf, int32_t nbytes){
+
+}   
+
+
+
+
+int32_t open(const uint8_t* filename){
+
+} 
+
+
+
+
+int32_t close(int32_t fd){
+
+}    
--- student-distrib/sys_call_helper.h
+++ student-distrib/sys_call_helper.h
@@ -2,6 +2,39 @@
 #define _SYS_CALL_HELPER_H
 
 #include "types.h"
+#include "file_system.h"
+#include "terminal.h"
+#include "rtc.h"
+#include "lib.h"
+
+#define FD_ARRAY_SIZE       8
+#define PROGRAM_ENTRY       24      // program eip starts at byte 24
+#define ENTRY_ADDR_SIZE     4       // program eip range [24,27]
+#define ELF_MAGIC_SIZE      4
+
+
+
+typedef struct file_descriptor{
+    uint32_t * file_op_ptr;  /* pointer to a struct of function pointer 
+                              * to this specific file type */
+    uint32_t inode;     /* this hold the index of inode for this file */
+    uint32_t* file_pos; /* the current byte you are reading within the file */
+    uint32_t flags;     /* the flag indicates whether this file is in 
+                         * use or closed. Current implementation is that
+                         * in use: flags != 0
+                         * closed: flags = 0    */
+}file_descriptor_t;
+
+typedef struct pcb{
+    uint32_t pid;                   // process id 
+    uint32_t parent_pid;            // or uint32_t* paren_pcb 
+    // uint32_t * process_addr;
+    file_descriptor_t[FD_ARRAY_SIZE]; // file descriptor array for the current process
+}pcb_t;
+
+
+
+
 
 // handle system call for checkpoint 1
 extern void system_call_helper();
@@ -23,5 +56,18 @@ extern int32_t getargs(uint8_t* buf, int32_t nbytes);   // 7
 extern int32_t vidmap(uint8_t** screen_start);    // 8
 extern int32_t set_handler(int32_t signum,void*handler_address); // 9
 extern int32_t sigreturn(void);   // 10
+// file operations table
+typedef struct Fod {
+    int32_t (*read)(int32_t fd, void* buf, int32_t nbytes);
+    int32_t (*write)(int32_t fd, const void* buf, int32_t nbytes);
+    int32_t (*open)(const uint8_t* filename);
+    int32_t (*close)(int32_t fd);
+} fod;
 
+fod rtc_fod {
+    .read = ,
+    .write = ,
+    .open = ,
+    .close =
+}
 #endif
